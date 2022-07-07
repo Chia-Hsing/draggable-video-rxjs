29e687d47148eae15f9c4386e47ceac35c099b60
chiahsinglo <ausgeflippte@gmail.com>
2022-07-07 23:50:58 +0800

    pagination

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-const { map, fromEvent, takeUntil, concatAll, withLatestFrom, filter } = require('rxjs');
+import { EMPTY, bufferTime, map, mergeAll, fromEvent, takeUntil, concatAll, withLatestFrom, filter, startWith, scan, merge } from 'rxjs';
 
-const video = document.getElementById('video');
-const anchor = document.getElementById('anchor');
+const video = document.getElementById('video')!;
+const anchor = document.getElementById('anchor')!;
 
 fromEvent(document, 'scroll')
-    .pipe(map((event) => anchor.getBoundingClientRect().bottom < 0))
-    .subscribe((isBottom) => {
+    .pipe(map((_event: any) => anchor.getBoundingClientRect().bottom < 0))
+    .subscribe((isBottom: any) => {
         if (isBottom) video.classList.add('video-fixed');
         else video.classList.remove('video-fixed');
     });
@@ -14,7 +14,7 @@ const mouseDown = fromEvent(video, 'mousedown');
 const mouseUp = fromEvent(document, 'mouseup');
 const mouseMove = fromEvent(document, 'mousemove');
 
-const validValue = (value, max, min) => {
+const validValue = (value: any, max: any, min: any) => {
     return Math.min(Math.max(value, min), max);
 };
 
@@ -22,18 +22,18 @@ const videoHeight = video.getBoundingClientRect().height;
 const videoWidth = video.getBoundingClientRect().width;
 
 mouseDown
-    .pipe(filter((event) => video.classList.contains('video-fixed')))
+    .pipe(filter((_event: any) => video.classList.contains('video-fixed')))
     .pipe(
-        map((event) => mouseMove.pipe(takeUntil(mouseUp))),
+        map((_event: any) => mouseMove.pipe(takeUntil(mouseUp))),
         concatAll()
     )
     .pipe(
-        withLatestFrom(mouseDown, (move, down) => ({
+        withLatestFrom(mouseDown, (move: any, down: any) => ({
             x: validValue(move.clientX - down.offsetX, window.innerWidth - videoWidth, 0),
             y: validValue(move.clientY - down.offsetY, window.innerHeight - videoHeight, 0),
         }))
     )
-    .subscribe((pos) => {
+    .subscribe((pos: any) => {
         video.style.top = pos.y + 'px';
         video.style.left = pos.x + 'px';
     });
